38e5875ef9fbee23173cb0cbceffea359a0bed07
VITOR PEREIRA <VOP1234@HOTMAIL.COM>
2023-05-02 20:00:50 -0300

    :sparkles: create Navbar UI

--- app/components/navbar/MenuItem.tsx
+++ app/components/navbar/MenuItem.tsx
@@ -1,17 +1,14 @@
-'use client';
+"use client";
 
 interface MenuItemProps {
   onClick: () => void;
   label: string;
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({
-  onClick,
-  label
-}) => {
-  return ( 
-    <div 
-      onClick={onClick} 
+const MenuItem: React.FC<MenuItemProps> = ({ onClick, label }) => {
+  return (
+    <div
+      onClick={onClick}
       className="
         px-4 
         py-3 
@@ -22,7 +19,7 @@ const MenuItem: React.FC<MenuItemProps> = ({
     >
       {label}
     </div>
-   );
-}
- 
+  );
+};
+
 export default MenuItem;
